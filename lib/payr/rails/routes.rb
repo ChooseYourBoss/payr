bbcfc129d207bb8ab987829fec9794cb201069dc
Ivan Schneider <contact@ivanschneider.fr>
2013-09-09 16:25:49 +0200

    routes change for more rails 4 compatibility

--- lib/payr/rails/routes.rb
+++ lib/payr/rails/routes.rb
@@ -1,20 +1,18 @@
 module ActionDispatch::Routing
-	class Mapper
-		def payr_routes(options={})
-				if options && options[:callback_controller]
-					get "#{options[:callback_controller]}/pay", as: "payr_bills_pay"
-					post "#{options[:callback_controller]}/pay", as: "payr_bills_pay"
-				else
-					get "payr/bills/pay", as: "payr_bills_pay"
-					post "payr/bills/pay", as: "payr_bills_pay"
-				end
-			%w(paid refused cancelled ipn failure).each do |action|
-				if options && options[:callback_controller]
-					get "#{options[:callback_controller]}/#{action}", as: "payr_bills_#{action}"
-				else
-					get "payr/bills/#{action}", as: "payr_bills_#{action}"
-				end
-			end
-		end
-	end
+  class Mapper
+    def payr_routes(options={})
+      if options && options[:callback_controller]
+        match "#{options[:callback_controller]}/pay", as: "payr_bills_pay", via: [:get, :post]
+      else
+        match "payr/bills/pay", as: "payr_bills_pay", via: [:get, :post]
+      end
+      %w(paid refused cancelled ipn failure).each do |action|
+        if options && options[:callback_controller]
+          get "#{options[:callback_controller]}/#{action}", as: "payr_bills_#{action}"
+        else
+          get "payr/bills/#{action}", as: "payr_bills_#{action}"
+        end
+      end
+    end
+  end
 end
